cfa381300e8043aff1a93f3672803a0b870fa78f
calisayadavid <calisayardavid@gmail.com>
2021-06-14 10:47:52 -0300

    primera version

---
+++ src/main/java/com/unju/grafico/Principal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unju.grafico;
+
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+/**
+ *
+ * @author D.C
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+        DefaultPieDataset datos = new DefaultPieDataset();
+        datos.setValue("Windows", new Double(70));
+        datos.setValue("iOS", new Double(15));
+        datos.setValue("Linux", new Double(10));
+        datos.setValue("XX", new Double(5));
+
+        JFreeChart grafico = ChartFactory.createPieChart(// graf de tipo circular
+                "Grafico Sistemas operativos",// título																		
+                datos, // datos
+                true, // incluir referenciasXX
+                true, false);
+
+        ChartPanel panel = new ChartPanel(grafico);
+
+        JFrame ventana = new JFrame("Grafica estadístico de uso en particular");
+        ventana.setVisible(true);
+        ventana.setSize(1000, 800);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ventana.add(panel);
+    }
+    
+}
