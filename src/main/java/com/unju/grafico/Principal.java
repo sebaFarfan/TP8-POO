fe7e35b96db67ae2aa6efd7b0bdbfddc10664899
yamil <yamil@192.168.2.104>
2021-06-17 19:18:06 -0300

    Modificado Por Farfan Yamil

--- src/main/java/com/unju/grafico/Principal.java
+++ src/main/java/com/unju/grafico/Principal.java
@@ -19,13 +19,13 @@ public class Principal {
     public static void main(String[] args) {
 
         DefaultPieDataset datos = new DefaultPieDataset();
-        datos.setValue("Windows", new Double(70));
-        datos.setValue("iOS", new Double(15));
-        datos.setValue("Linux", new Double(10));
-        datos.setValue("XX", new Double(5));
+        datos.setValue("Internacional", new Double(70));
+        datos.setValue("Sociedad", new Double(15));
+        datos.setValue("Cronica social", new Double(10));
+        datos.setValue("Deportes", new Double(5));
 
         JFreeChart grafico = ChartFactory.createPieChart(// graf de tipo circular
-                "Grafico Sistemas operativos",// título																		
+                "Visitas de las redes sociales",// título																		
                 datos, // datos
                 true, // incluir referenciasXX
                 true, false);
